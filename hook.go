e7a52e36e42f8e27266d30adc5b0ca23b549d56c
santi tan <pongsanti.tanvejsilp@gmail.com>
2019-04-23 12:10:32 +0700

    before insert hook

---
+++ hook.go
@@ -0,0 +1,26 @@
+package content
+
+import (
+	"context"
+
+	"github.com/pongsanti/content/db/models"
+	"github.com/volatiletech/null"
+	"github.com/volatiletech/sqlboiler/boil"
+)
+
+func createBeforeInsertHookFunc(contextKey uint) func(context.Context, boil.ContextExecutor, *models.Content) error {
+	return func(ctx context.Context, exec boil.ContextExecutor, content *models.Content) error {
+		creatorID, ok := ctx.Value(contextKey).(int)
+		if ok {
+			content.CreatorID = null.IntFrom(creatorID)
+		}
+		return nil
+	}
+}
+
+/*
+	RegisterBeforeInsertHook receives contextKey of the creator id
+*/
+func RegisterBeforeInsertHook(contextKey uint) {
+	models.AddContentHook(boil.BeforeInsertHook, createBeforeInsertHookFunc(contextKey))
+}
